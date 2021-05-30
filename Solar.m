3a8b0820cc98640b1f838b9670c496dadd7dbf95
Emirhan Uzuner <emirhannuz@gmail.com>
2021-05-30 20:35:36 +0300

    Update Solar.m

--- Solar.m
+++ Solar.m
@@ -35,22 +35,40 @@ classdef Solar < handle
         end
         
         function [] = calculateHourAngle(obj)
+            % (AST - 12) * 15
             % Bu metod AST değerinin ondalıklı değeri ile çalışmalı
+            % Hesaplanan değer ilgili property'ye atanır.
+            % Boş bir dizi geri döndürür. Bizim için void anlamına geldiğini varsayabiliriz.
             obj.HourAngle = (obj.Ast - 12) * 15;
         end
         
         function [] = calculateDeclination(obj)
             % δ = 23.45 * sin(360/365 * (284 + N))
             % N -> Yılın günü
-            obj.Declination = 23.45 * sind(360/365 * (284 + obj.DayOfTheYear));
+            % Yılın herhangi bir günü için derece cinsinden sapmayı hesaplar ve ilgili property'ye atar.
+            % Boş bir dizi geri döndürür. Bizim için void anlamına geldiğini varsayabiliriz.
+            obj.Declination = 23.45 * sind(360 / 365 * (284 + obj.DayOfTheYear));
         end
        
         function [] = calculateAltitude(obj)
+            % Güneş ışınları ile yatay düzlem arasındaki açıdır.
+            % Hesaplanan bu değer ilgili property'ye atanır.
+            % Hesaplamalar derece olarak sonuç verdiğinden sind, cosd veya asind gibi d ile biten fonksiyonlar
+            % gerekli hesaplamayı yaparken gönderilen değerlerin derece olduğunu kabul edip işlemler yapar.
+            % Boş bir dizi geri döndürür. Bizim için void anlamına geldiğini varsayabiliriz.
+            % Neden arcsinus ? Hesaplanan değerin formülde a'ya değil de sin(a)'ya eşit olduğu söylendiğinden 
+            % bu sin(a)'nın kaç derece olduğunu bulabilmek için arcsin kullanıldı.
             a  = sind(obj.Latitude) * sind(obj.Declination) + cosd(obj.Latitude) * cosd(obj.Declination) * cosd(obj.HourAngle);
             obj.Altitude = asind(a);
         end
         
         function [] = calculateAzimuth(obj)
+            % Kuzey Yarımküre için güneyden veya Güney Yarımküre için kuzeyden itibaren yatay düzlemde ölçülen güneş 
+            % ışınlarının açısıdır.
+            % Hesaplanan bu değer ilgili property'ye atanır.
+            % Neden arcsinus ? Hesaplanan değerin formülde a'ya değil de sin(a)'ya eşit olduğu söylendiğinden 
+            % bu sin(a)'nın kaç derece olduğunu bulabilmek için arcsin kullanıldı.
+            % Boş bir dizi geri döndürür. Bizim için void anlamına geldiğini varsayabiliriz.
             z =  cosd(obj.Declination) * sind(obj.HourAngle) / cosd(obj.Altitude);
             obj.Azimuth = asind(z);
         end
